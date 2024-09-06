9db393f012648d65c0eac18142e39ecd15b7a58a
nh-maker <nadahaninou98@gmail.com>
2024-09-06 12:23:13 +0100

    Message décrivant les modifications4

--- src/javaapplication10/AdministrationCamionette.java
+++ src/javaapplication10/AdministrationCamionette.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package javaapplication10;
-
+import java.sql;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
